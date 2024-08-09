0b8b826d4bec1c4c2ffc1764f315d29df7cc1e8f
Rafael Teixeira <rafaelteixeiravl@gmail.com>
2024-08-09 15:37:13 -0300

    feat: adiciona a traducao da paginacao

---
+++ Crud/resources/lang/pt-br/pagination.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'showing' => 'Mostrando',
+    'previous' => '&laquo; Anterior',
+    'next' => 'Próximo &raquo;',
+    'results' => 'resultados',
+    'to' => 'de',
+    'of' => 'para',
+];
