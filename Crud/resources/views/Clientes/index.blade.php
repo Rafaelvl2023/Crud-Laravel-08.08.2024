3734d32c245b1593685bb4ae67d8f487dad81827
Rafael Teixeira <rafaelteixeiravl@gmail.com>
2024-08-09 10:44:27 -0300

    feat: adiciona o estilo bootstrap na paginaticao

--- Crud/resources/views/Clientes/index.blade.php
+++ Crud/resources/views/Clientes/index.blade.php
@@ -80,6 +80,7 @@
         }
 
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
@@ -121,7 +122,7 @@
             @endforeach
         </table>
         <div class="paginacao">
-            {{ $clientes->onEachSide(0)->links('pagination::bootstrap-4') }}
+            {{ $clientes->onEachSide(0)->links() }}
         </div>
     </div>
 </body>
